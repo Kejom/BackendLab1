fd87027ee2e37be021c0afce3cb18f3bbd0392b7
artur.zorawski <artur.zorawski@PK1-07>
2022-03-13 10:37:07 +0100

    Dodaj pliki projektów.

---
+++ BackendLab1/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendLab1
+{
+    class BinarySearch
+    {
+        public int BinarySort(List<int> listToSearch, int valToSearch)
+        {
+            if (listToSearch.Count == 0)
+                return -1;
+
+            var index = 0;
+            var limit = listToSearch.Count - 1;
+
+            while (limit - index > 1)
+            {
+                var point = (int)Math.Ceiling((double)(index + limit) / 2);
+                var pointVal = listToSearch[point];
+
+                if (pointVal == valToSearch)
+                    return point;
+
+                if (valToSearch < pointVal)
+                    limit = point;
+                else
+                    index = point;
+            }
+            if (listToSearch[index] == valToSearch)
+                return index;
+            return -1;
+        }
+    }
+}
